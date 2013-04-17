acbc70c12906150045bfe742d0d709b33616c1fb
Richard Vowles <richard@bluetrainsoftware.com>
2013-04-17 20:58:47 +1200

    fix Windows runner

--- src/main/groovy/com/bluetrainsoftware/maven/karma/KarmaBaseMojo.groovy
+++ src/main/groovy/com/bluetrainsoftware/maven/karma/KarmaBaseMojo.groovy
@@ -175,7 +175,7 @@ class KarmaBaseMojo extends AbstractMojo {
     ProcessBuilder builder
 
     if (File.separator == '\\') // Windows
-      builder = new ProcessBuilder("cmd", "/C karma start " + configFile.absolutePath)
+      builder = new ProcessBuilder("cmd", "/C", "karma", "start", configFile.absolutePath)
     else
       builder = new ProcessBuilder("karma", "start", configFile.absolutePath);
 
